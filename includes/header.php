02cd64e5972d508de40bd7e6d184a03b93296e79
Atul Raj <113578617+atulraj10481@users.noreply.github.com>
2025-09-06 12:13:54 +0530

    GTM Updated

--- includes/header.php
+++ includes/header.php
@@ -90,17 +90,13 @@
     </a>
 </div>
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-3WXT93SHRG"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-    gtag('config', 'G-3WXT93SHRG');
-</script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PGGNC3QT');</script>
+<!-- End Google Tag Manager -->
 
 <!-- Add Bootstrap JS at the end of body -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
